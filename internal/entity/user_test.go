b37dd5aa022ca396f8636ca398ff6c512ab46ab4
Thiago <thiago.dev.x@gmail.com>
2025-03-10 13:57:07 -0300

    update

--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -1,21 +1,27 @@
 package entity
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewUser(t *testing.T) {
 	email := "test@test.com"
 	password := "password"
 	user, err := NewUser(email, password)
-	if err != nil {
-		t.Error(err)
-	}
-	if user.Email != email {
-		t.Errorf("Email is not correct")
-	}
-	if !user.ValidatePassword(password) {
-		t.Errorf("Password is not correct")
-	}
-	if user.Password == password {
-		t.Errorf("Password should be hashed")
-	}
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.Equal(t, email, user.Email)
+	assert.True(t, user.ValidatePassword(password))
+	assert.NotEqual(t, password, user.Password)
+}
+
+func TestUser_ValidatePassword(t *testing.T) {
+	email := "t@t.com"
+	password := "password"
+	user, _ := NewUser(email, password)
+	assert.True(t, user.ValidatePassword(password))
+	assert.False(t, user.ValidatePassword("wrong"))
+	assert.NotEqual(t, password, user.Password)
 }
