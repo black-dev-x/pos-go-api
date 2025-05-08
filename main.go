0dfeaa2b3bc2a6094a4f52e68acf42f5fb2f0de1
Thiago <thiago.dev.x@gmail.com>
2025-05-08 16:19:48 -0300

    update

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/black-dev-x/pos-go-api/configs"
+import (
+	"net/http"
+
+	"github.com/black-dev-x/pos-go-api/configs"
+	"github.com/black-dev-x/pos-go-api/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
 	configs.LoadConfig()
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8080", nil)
 }
