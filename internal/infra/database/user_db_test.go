0e548d04cd77e60647399f86cef799bc63b56d81
Thiago <thiago.dev.x@gmail.com>
2025-05-08 13:31:45 -0300

    update

---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/black-dev-x/pos-go-api/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error("failed to connect to database:", err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("john@doe.com", "12345")
+	userDB := NewUser(db)
+
+	error := userDB.Create(user)
+	assert.Nil(t, error)
+
+	userFound, err := userDB.FindByEmail("john@doe.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, userFound)
+}
