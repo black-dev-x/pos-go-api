58b90ce140aba5cfac3247fda041a8a9fdd5c7f5
Thiago <thiago.dev.x@gmail.com>
2025-05-08 12:19:22 -0300

    test

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -14,3 +14,24 @@ func TestNewProduct(t *testing.T) {
 	assert.Equal(t, "Test Product", p.Name)
 	assert.Equal(t, 10.0, p.Price)
 }
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+	p, err := NewProduct("", 10.0)
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrorNameIsRequired, err)
+	assert.Nil(t, p)
+}
+
+func TestProductWhenPriceIsZero(t *testing.T) {
+	p, err := NewProduct("Test Product", 0)
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrorPriceIsInvalid, err)
+	assert.Nil(t, p)
+}
+
+func TestProductWhenPriceIsNegative(t *testing.T) {
+	p, err := NewProduct("Test Product", -1)
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrorPriceIsInvalid, err)
+	assert.Nil(t, p)
+}
