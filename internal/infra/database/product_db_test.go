38a751eb2ecf08585bfcd4ce92423395c5a1d439
Thiago <thiago.dev.x@gmail.com>
2025-05-08 16:16:01 -0300

    test

--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -11,6 +11,47 @@ import (
 	"gorm.io/gorm"
 )
 
+func TestUpgradeProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	assert.Nil(t, err)
+	db.AutoMigrate(&entity.Product{})
+	productDB := NewProductDB(db)
+
+	product, error := entity.NewProduct("Test Product", 10.0)
+	assert.Nil(t, error)
+
+	error = productDB.Create(product)
+	assert.Nil(t, error)
+
+	product.Name = "Updated Product"
+	error = productDB.Update(product)
+	assert.Nil(t, error)
+
+	updatedProduct, error := productDB.FindByID(product.ID.String())
+	assert.Nil(t, error)
+	assert.Equal(t, "Updated Product", updatedProduct.Name)
+}
+
+func TestDeleteProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	assert.Nil(t, err)
+	db.AutoMigrate(&entity.Product{})
+	productDB := NewProductDB(db)
+
+	product, error := entity.NewProduct("Test Product", 10.0)
+	assert.Nil(t, error)
+
+	error = productDB.Create(product)
+	assert.Nil(t, error)
+
+	error = productDB.Delete(product.ID.String())
+	assert.Nil(t, error)
+
+	product, error = productDB.FindByID(product.ID.String())
+	assert.NotNil(t, error)
+	assert.Nil(t, product)
+}
+
 func TestFindProductByID(t *testing.T) {
 	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
 	assert.Nil(t, err)
